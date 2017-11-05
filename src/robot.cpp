d8717584c8bb122fefdc811c14aa8a4d2cf7f3cd
TeoKa <matteo_ciocca@yahoo.it>
2017-11-05 17:41:22 +0100

    cleaning the code

--- src/robot.cpp
+++ src/robot.cpp
@@ -36,33 +36,24 @@ double robot::get_dz(){
     return vel_z;
 }
 
-void robot::place(double x,double y,double z,double dx,double dy,double dz){
-    
-    // Used to place (initialize) the robot in the space
-    pos_x = x;
-    pos_y = y;
-    pos_z = z;
-    vel_x = dx;
-    vel_y = dy;
-    vel_z = dz;
-    
-    return;
-}
+// Used to place (initialize) the robot in the space
+void robot::place(double x,double y,double z,double dx,double dy,double dz)
+  : pos_x(x), pos_y(y), pos_z(z), vel_x(dx), vel_y(dy), vel_z(dz) {}
 
 
 void robot::move(double dt,double t_end,double p,double xt,double yt,double zt,double xi,double yi,double zi,trajectory_planner* planner){
-    
+
     double d_pos;
     double d_vel;
     double abs_f_i;
-    
+
     d_pos = planner->polynomial_position(dt,t_end,p);
     d_vel = planner->polynomial_velocity(dt,t_end,p);
 
     abs_f_i = planner->delta_space_f_i(xt,yt,zt,xi,yi,zi);
-    
+
     // From a polynomial time-law to a trajectory in the 3D space, w.r.t. three components (px,py,pz). In this case a rectilinear path in the 3D space (pag. 182 of [2]).
-    
+
     // position
     pos_x = xi + d_pos*(xt-xi)/abs_f_i;
     pos_y = yi + d_pos*(yt-yi)/abs_f_i;
@@ -72,12 +63,7 @@ void robot::move(double dt,double t_end,double p,double xt,double yt,double zt,d
     vel_x = d_vel*(xt-xi)/abs_f_i;
     vel_y = d_vel*(yt-yi)/abs_f_i;
     vel_z = d_vel*(zt-zi)/abs_f_i;
-    
+
     return;
-    
+
 }
-
-
-
-
-
